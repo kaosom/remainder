3430faf9317a0e61ae3371f3148613c223379a68
= <=>
2024-06-19 14:34:51 -0600

    adding port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const { initializeFirebase } = require('./config/firebase');
 const { scheduleCronJobs } = require('./cronJobs');
+const express = require('express');
 
-// Inicializa Firebase
+// Initialize Firebase
 initializeFirebase();
 
-// Programa las tareas cron
+// Schedule cron jobs
 scheduleCronJobs();
 
-console.log('Servidor iniciado y tarea cron configurada');
+console.log('Server started and cron job scheduled');
+
+// Create an Express app
+const app = express();
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Cron job server is running');
+});
+
+// Listen on the port specified by Render
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
